a7d82c37c725ec54c0210ae78c858939c9fd3d57
이정은 <2019152052@kpu.ac.kr>
2023-02-08 19:30:57 +0900

    fix:  관심, 공유 아이콘 추가, 폰트 사이즈 등 디테일 수정

--- src/pages/ProjectDetailPage.tsx
+++ src/pages/ProjectDetailPage.tsx
@@ -1,40 +1,88 @@
 import styled from '@emotion/styled';
+import WebContainer from '../components/common/WebContainer';
+import { RiHeartAddLine, RiHeartAddFill, RiShareBoxLine } from 'react-icons/ri';
 
 const ProjectDetailPage = () => {
   return (
     <ProjectDetailContainer>
-      <ProjectDetailDiv>
-        <ProjectTitle>모집중 프로젝트 명입니다.</ProjectTitle>
-        <ProjectThumbnail></ProjectThumbnail>
-        <WriterAndShareContainer>
-          <WriterWrapper>
-            <WriterProfileImg />
-            <WriterNickname>닉네임입니다. 닉네임입니다.</WriterNickname>
-          </WriterWrapper>
-          <IconWrapper>조회 00 관심 00 공유 </IconWrapper>
-        </WriterAndShareContainer>
-        <RecruitmentInfoContainer>
-          <ProjectInfoWrapper>
-            <ProjectInfo>모집인원: 1명</ProjectInfo>
-            <ProjectInfo>필요스택: 기획 프론트 백엔드 디자인</ProjectInfo>
-            <ProjectInfo>예상기간: 2021.01.01 ~ 2021.01.01</ProjectInfo>
-            <ProjectInfo>근무지: 서울 강남구</ProjectInfo>
-          </ProjectInfoWrapper>
-          <MemberInfoWrapper>
-            <MemberInfoTitle>현재 참여중인 인원</MemberInfoTitle>
-            <MemberInfoBox>
-              <MemberInfo>기획 동그라미 동그라미</MemberInfo>
-              <MemberInfo>기획 동그라미 동그라미</MemberInfo>
-              <MemberInfo>기획 동그라미 동그라미</MemberInfo>
-            </MemberInfoBox>
-          </MemberInfoWrapper>
-        </RecruitmentInfoContainer>
-        <RecruitContentsContainer>
-          <ContentTitle>모집 안내</ContentTitle>
-          <ContentWrapper>내용입니당</ContentWrapper>
-        </RecruitContentsContainer>
-        <ApplyButton>간단 지원하기</ApplyButton>
-      </ProjectDetailDiv>
+      <WebContainer>
+        <ProjectDetailWrapper>
+          <ProjectTitleWrapper>
+            <RecruitmentDiv>모집중</RecruitmentDiv>
+            <ProjectTitle>프로젝트 명입니다.</ProjectTitle>
+          </ProjectTitleWrapper>
+          <ProjectThumbnail />
+          <WriterAndShareContainer>
+            <WriterWrapper>
+              {/* 게시글 작성자 프로필 이미지/ Todo: 클릭시 공개프로필로 연결 */}
+              <WriterProfileImg />
+              <WriterNickname>내 이름은 고난, 탐정이죠</WriterNickname>
+            </WriterWrapper>
+            <IconWrapper>
+              조회 131
+              <IconButton>
+                {/* Todo: 관심버튼 false->true이면 RiHeartAddFill아이콘으로 변경해주고 +1, true->false이면 RiHeartAddLine아이콘으로 변경해주고 -1*/}
+                <RiHeartAddLine />
+                관심 00
+              </IconButton>
+              {/* Todo: 공유기능 추가하기 */}
+              <IconButton>
+                <RiShareBoxLine />
+                공유
+              </IconButton>
+            </IconWrapper>
+          </WriterAndShareContainer>
+          <RecruitmentInfoContainer>
+            <ProjectInfoWrapper>
+              <ProjectInfoObject>
+                <ProjectInfoKey>모집인원</ProjectInfoKey>
+                <ProjectInfoValue> 00명</ProjectInfoValue>
+              </ProjectInfoObject>
+              <ProjectInfoObject>
+                <ProjectInfoKey>필요스택</ProjectInfoKey>
+                <ProjectInfoValue> 기획 개발 디자인</ProjectInfoValue>
+              </ProjectInfoObject>
+              <ProjectInfoObject>
+                <ProjectInfoKey>예상기간</ProjectInfoKey>
+                <ProjectInfoValue> 2023.01.01~2023.01.01</ProjectInfoValue>
+              </ProjectInfoObject>
+              <ProjectInfoObject>
+                <ProjectInfoKey>근무지</ProjectInfoKey>
+                <ProjectInfoValue> 서울시 강남구</ProjectInfoValue>
+              </ProjectInfoObject>
+            </ProjectInfoWrapper>
+            <MemberInfoWrapper>
+              <MemberInfoTitle>현재 참여중인 인원</MemberInfoTitle>
+              <MemberInfoBox>
+                <MemberInfoDiv>
+                  <PositionDiv>기획</PositionDiv>
+                  {/*  Todo: 클릭시 공개프로필로 연결 */}
+                  <MemberProfileImg />
+                  <MemberProfileImg />
+                </MemberInfoDiv>
+                <MemberInfoDiv>
+                  <PositionDiv>개발</PositionDiv>
+                  <MemberProfileImg />
+                  <MemberProfileImg />
+                  <MemberProfileImg />
+                </MemberInfoDiv>
+                <MemberInfoDiv>
+                  <PositionDiv>디자인</PositionDiv>
+                  <MemberProfileImg />
+                  <MemberProfileImg />
+                </MemberInfoDiv>
+              </MemberInfoBox>
+            </MemberInfoWrapper>
+          </RecruitmentInfoContainer>
+          <RecruitContentsContainer>
+            <ContentTitle>모집 안내</ContentTitle>
+            <ContentWrapper>내용입니당</ContentWrapper>
+          </RecruitContentsContainer>
+          <ButtonWrapper>
+            <ApplyButton>간단 지원하기</ApplyButton>
+          </ButtonWrapper>
+        </ProjectDetailWrapper>
+      </WebContainer>
     </ProjectDetailContainer>
   );
 };
@@ -43,31 +91,46 @@ export default ProjectDetailPage;
 
 const ProjectDetailContainer = styled.div`
   background-color: #fcfcfc;
+  height: 163.25rem;
 `;
 
-const ProjectDetailDiv = styled.div`
-  width: 1180px;
+const ProjectDetailWrapper = styled.div`
+  width: 73.75rem;
   display: flex;
   flex-direction: column;
-  margin: 150px auto;
+  margin: 0 auto;
+`;
+
+const ProjectTitleWrapper = styled.div`
+  margin-top: 16.125rem;
+  display: flex;
+  align-items: center;
+  gap: 0.625rem;
+`;
+
+const RecruitmentDiv = styled.div`
+  background-color: #6f64f2;
+  color: #fff;
+  padding: 0.625rem 1.875rem;
+  border-radius: 2.5rem;
 `;
 
 const ProjectTitle = styled.div`
   font-size: 1.5rem;
-  font-weight: 600;
-  margin-bottom: 1rem;
+  font-weight: 400;
 `;
 
 const ProjectThumbnail = styled.div`
-  width: 1180px;
-  height: 584px;
+  width: 73.75rem;
+  height: 36.5rem;
+  margin-top: 1rem;
   background-color: #dadada;
 `;
 
 const WriterAndShareContainer = styled.div`
   display: flex;
   justify-content: space-between;
-  margin-top: 34px;
+  margin-top: 2.125rem;
 `;
 
 const WriterWrapper = styled.div`
@@ -76,13 +139,21 @@ const WriterWrapper = styled.div`
 `;
 
 const IconWrapper = styled.div`
+  width: 279px;
   display: flex;
   align-items: center;
+  justify-content: space-between;
+`;
+
+const IconButton = styled.button`
+  display: flex;
+  align-items: center;
+  gap: 0.5rem;
 `;
 
 const WriterProfileImg = styled.img`
-  width: 40px;
-  height: 40px;
+  width: 2.5rem;
+  height: 2.5rem;
   border-radius: 50%;
   background-color: #aaaaaa;
 `;
@@ -97,64 +168,112 @@ const WriterNickname = styled.p`
 const RecruitmentInfoContainer = styled.div`
   display: flex;
   flex-direction: column;
-  width: 1180px;
-  height: 693px;
+  width: 73.75rem;
+  height: 43.3125rem;
   background-color: #fff;
-  margin-top: 56px;
+  margin-top: 3.5rem;
   align-items: center;
+  padding: 40px;
 `;
 
 const ProjectInfoWrapper = styled.div`
-  width: 1018px;
-  height: 200px;
-  margin-top: 40px;
+  width: 63.625rem;
+  height: 12.5rem;
+  font-size: 20px;
   display: flex;
   flex-direction: column;
   justify-content: space-between;
 `;
 
-const ProjectInfo = styled.div``;
+const ProjectInfoObject = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 45px;
+`;
+
+const ProjectInfoKey = styled.div`
+  width: 88px;
+`;
+
+const ProjectInfoValue = styled.div``;
 
 const MemberInfoWrapper = styled.div`
-  width: 1018px;
-  height: 364px;
-  margin-top: 44px;
+  width: 63.625rem;
+  height: 22.75rem;
+  margin-top: 2.75rem;
   display: flex;
   flex-direction: column;
 `;
 
 const MemberInfoTitle = styled.div`
-  height: 48px;
+  height: 32px;
+  font-weight: 500;
+  font-size: 20px;
 `;
 
 const MemberInfoBox = styled.div`
   height: 100%;
-  margin: 20px 200px 20px 0;
+  margin: 36px 0 1.25rem 0;
   display: flex;
   flex-direction: column;
   justify-content: space-between;
 `;
 
-const MemberInfo = styled.div``;
-
-const RecruitContentsContainer = styled.div`
-  margin-top: 58px;
+const MemberInfoDiv = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 1.25rem;
 `;
 
-const ContentTitle = styled.div``;
+const PositionDiv = styled.div`
+  font-size: 24px;
+  background-color: #6f64f2;
+  width: 127px;
+  height: 40px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  color: #fff;
+  border-radius: 18px;
+`;
+
+const MemberProfileImg = styled.img`
+  width: 64px;
+  height: 64px;
+  border-radius: 50%;
+  background-color: #aaaaaa;
+`;
+
+const RecruitContentsContainer = styled.div`
+  margin-top: 3.625rem;
+`;
+
+const ContentTitle = styled.div`
+  font-weight: 500;
+  font-size: 24px;
+`;
 
 const ContentWrapper = styled.div`
-  width: 1180px;
-  height: 532px;
-  margin-top: 27px;
+  width: 73.75rem;
+  height: 33.25rem;
+  margin-top: 1.6875rem;
   background-color: #fff;
-  padding: 40px;
+  padding: 2.5rem;
+  font-size: 20px;
+`;
+
+const ButtonWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-top: 3.5rem;
 `;
 
 const ApplyButton = styled.button`
-  margin-top: 56px;
-  width: 521px;
-  height: 88px;
+  width: 32.5625rem;
+  height: 5.5rem;
   background: #6f64f2;
-  border-radius: 36px;
+  border-radius: 2.25rem;
+  font-size: 1.75rem;
+  color: #fff;
 `;
