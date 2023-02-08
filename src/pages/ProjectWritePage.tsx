de7e13adf661a81f8f082b413fabddc5623b7619
vpvm96 <vpvm96@gmail.com>
2023-02-08 18:09:39 +0900

    feat: 글 작성 메인 페이지 생성

--- src/pages/ProjectWritePage.tsx
+++ src/pages/ProjectWritePage.tsx
@@ -1,5 +1,23 @@
+import WebContainer from 'components/common/WebContainer';
+import ProjectWritePageBody from 'components/writepage/ProjectWritePageBody';
+import ProjectWritePageFooter from 'components/writepage/ProjectWritePageFooter';
+import ProjectWritePageHeader from 'components/writepage/ProjectWritePageHeader';
+import styled from '@emotion/styled';
+
 const ProjectWritePage = () => {
-  return <div>ProjectWritePage</div>;
+  return (
+    <WebContainer>
+      <ProjectWritePageWrapper>
+        <ProjectWritePageHeader />
+        <ProjectWritePageBody />
+        <ProjectWritePageFooter />
+      </ProjectWritePageWrapper>
+    </WebContainer>
+  );
 };
+
+const ProjectWritePageWrapper = styled.div`
+  height: 133.5625rem;
+`;
 
 export default ProjectWritePage;
