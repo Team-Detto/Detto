62d62da6cce6d81223e015b9aa2b36ee40e933f9
vpvm96 <vpvm96@gmail.com>
2023-02-08 18:10:53 +0900

    feat: 클릭시 색상 변경되는 스킬 버튼 생성

---
+++ src/components/common/SkillButton.tsx
@@ -0,0 +1,38 @@
+import { useState } from 'react';
+import styled from '@emotion/styled';
+
+interface props {
+  name: string;
+}
+
+const SkillButton = ({ name }: props) => {
+  const [isActive, setIsActive] = useState(false);
+
+  const handleActiveButton = () => {
+    setIsActive(!isActive);
+  };
+
+  return (
+    <SkillButtonContainer>
+      <SkillBtn isActive={isActive} onClick={handleActiveButton}>
+        {name}
+      </SkillBtn>
+    </SkillButtonContainer>
+  );
+};
+
+const SkillButtonContainer = styled.div``;
+const SkillBtn = styled.button`
+  background-color: ${(props: { isActive: boolean }) =>
+    props.isActive === true ? '#5D50F0' : '#f2f4f6'};
+  border-radius: 32px;
+  padding: 5px 15px;
+  transform: scale(1);
+  transition: transform 0.5s;
+  &:hover {
+    transform: scale(1.1);
+    transition: transform 0.5s;
+  }
+`;
+
+export default SkillButton;
