ac00479c5330fd9c4d38f2f3e715a0a8588b4996
vpvm96 <vpvm96@gmail.com>
2023-02-09 15:23:06 +0900

    feat: writeForm onChage 구현

--- src/pages/ProjectWritePage.tsx
+++ src/pages/ProjectWritePage.tsx
@@ -1,3 +1,5 @@
+import React from 'react';
+import { useState } from 'react';
 import useModal from 'hooks/useModal';
 import ConfirmAlert from 'components/common/ConfirmAlert';
 import WebContainer from 'components/common/WebContainer';
@@ -6,14 +8,49 @@ import ProjectWritePageFooter from 'components/writepage/ProjectWritePageFooter'
 import ProjectWritePageHeader from 'components/writepage/ProjectWritePageHeader';
 import styled from '@emotion/styled';
 
+export interface WriteFormValueType {
+  [key: string]: string | number | string[];
+}
+
 const ProjectWritePage = () => {
   const { isOpen, handleOpenButtonClick, handleCloseButtonClick } =
     useModal(false);
+
+  const [writeFormValue, setWriteFormValue] = useState({
+    title: '',
+    content: '',
+    plannerPosition: 0,
+    developerPosition: 0,
+    designerPosition: 0,
+    plannerStack: [''],
+    developerStack: [''],
+    designerStack: [''],
+    startDate: '',
+    endDate: '',
+    deadline: '',
+  });
+
+  const handleFormValueChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setWriteFormValue((prev) => {
+      return {
+        ...prev,
+        [name]: value,
+      };
+    });
+  };
+
   return (
     <WebContainer>
       <ProjectWritePageWrapper isOpen={isOpen}>
-        <ProjectWritePageHeader />
-        <ProjectWritePageBody />
+        <ProjectWritePageHeader
+          writeFormValue={writeFormValue}
+          onFormValueChangeEvent={handleFormValueChange}
+        />
+        <ProjectWritePageBody
+          writeFormValue={writeFormValue}
+          onFormValueChagneEvent={handleFormValueChange}
+        />
         <ProjectWritePageFooter
           onOpenButtonClickEvent={handleOpenButtonClick}
         />
