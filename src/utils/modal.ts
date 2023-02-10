e673e3be40e6b50f57590f3a68ad426a08cab8bb
Hyojin <0910.shj@gmail.com>
2023-02-10 12:08:33 +0900

    fix: 모달이 열려있을 때 body 스크롤 방지 기능 개선
    * 기존 : 스크롤이 사라지면서 페이지가 16px만큼 움직임.
    * 개선 : 스크롤이 사라지지 않아 페이지 움직임이 없음.

---
+++ src/utils/modal.ts
@@ -0,0 +1,24 @@
+/**
+ * 스크롤을 방지하고 현재 위치를 반환한다.
+ * @returns {number} 현재 스크롤 위치
+ */
+export const preventScroll = () => {
+  const currentScrollY = window.scrollY;
+  document.body.style.position = 'fixed';
+  document.body.style.width = '100%';
+  document.body.style.top = `-${currentScrollY}px`; // 현재 스크롤 위치
+  document.body.style.overflowY = 'scroll';
+  return currentScrollY;
+};
+
+/**
+ * 스크롤을 허용하고, 스크롤 방지 함수에서 반환된 위치로 이동한다.
+ * @param prevScrollY 스크롤 방지 함수에서 반환된 스크롤 위치
+ */
+export const allowScroll = (prevScrollY: number) => {
+  document.body.style.position = '';
+  document.body.style.width = '';
+  document.body.style.top = '';
+  document.body.style.overflowY = '';
+  window.scrollTo(0, prevScrollY);
+};
