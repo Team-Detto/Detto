6dfa3b6e375c843ab08d44e9972c1b5de9509a16
vpvm96 <vpvm96@gmail.com>
2023-02-11 20:37:19 +0900

    fix: Type 따로 분리후 경로 재지정 및 적용

--- src/components/writepage/WritePagePosition.tsx
+++ src/components/writepage/WritePagePosition.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import { WriteFormValueType } from 'hooks/useWrite';
+import { WriteType } from 'types/write/writeType';
 import LabelInput from 'components/common/LabelInput';
 import styled from '@emotion/styled';
 interface Props {
-  writeFormValue: WriteFormValueType;
+  writeFormValue: WriteType.WriteFormType;
   onFormValueChagneEvent: (e: React.ChangeEvent<HTMLInputElement>) => void; // eslint-disable-line no-unused-vars
 }
 
