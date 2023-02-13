0892836abab077ab667bca8ef9e922b4f7e4bb91
vpvm96 <vpvm96@gmail.com>
2023-02-13 14:38:23 +0900

    fix: Figma에 디자인 수정으로 인한 디자인 수정 및 적용

--- src/pages/ProjectEditPage.tsx
+++ src/pages/ProjectEditPage.tsx
@@ -1,3 +1,5 @@
+import { useEffect, useState } from 'react';
+import { firebaseGetProjectDataRequest } from 'apis/boardService';
 import { useModal } from 'hooks';
 import ConfirmAlert from 'components/common/ConfirmAlert';
 import WebContainer from 'components/common/WebContainer';
@@ -5,8 +7,6 @@ import ProjectEditPageHeader from 'components/editpage/ProjectEditPageHeader';
 import ProjectEditPageBody from 'components/editpage/ProjectEditPageBody';
 import ProjectEditPageFooter from 'components/editpage/ProjectEditPageFooter';
 import styled from '@emotion/styled';
-import { useEffect, useState } from 'react';
-import { firebaseGetProjectDataRequest } from 'apis/boardService';
 
 const ProjectEditPage = () => {
   const { isOpen, handleModalStateChange } = useModal(false);
@@ -19,13 +19,14 @@ const ProjectEditPage = () => {
 
   return (
     <WebContainer>
-      <EditPageWrapper isOpen={isOpen}>
+      <EditPageWrapper>
         <ProjectEditPageHeader />
         <ProjectEditPageBody />
         <ProjectEditPageFooter />
         <ConfirmAlert
           isOpen={isOpen}
           message="게시글을 수정하시겠습니까?"
+          subMessage="수정한 게시글은 마이페이지에서 확인할 수 있습니다."
           onClickEvent={handleModalStateChange}
           onCloseEvent={handleModalStateChange}
         />
@@ -36,12 +37,6 @@ const ProjectEditPage = () => {
 
 const EditPageWrapper = styled.div`
   height: 133.5625rem;
-  position: ${(props: { isOpen: boolean }) =>
-    props.isOpen ? 'fixed' : 'static'};
-  top: ${(props: { isOpen: boolean }) =>
-    props.isOpen ? `-${window.scrollY}px` : '0'};
-  overflow-y: ${(props: { isOpen: boolean }) =>
-    props.isOpen ? 'scroll' : 'hidden'};
 `;
 
 export default ProjectEditPage;
