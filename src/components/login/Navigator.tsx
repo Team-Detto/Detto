ca162aa9fb07d865b8a4a09988f2016c2c1cb7a8
Hyojin <0910.shj@gmail.com>
2023-02-13 12:03:25 +0900

    feat: Navigator 컴포넌트 추가

---
+++ src/components/login/Navigator.tsx
@@ -0,0 +1,47 @@
+import { IoChevronBack } from 'react-icons/io5';
+import { CgClose } from 'react-icons/cg';
+import COLORS from 'assets/styles/colors';
+import styled from '@emotion/styled';
+import { useLoginModal } from 'hooks';
+
+type NavigatorProps = {
+  page: number;
+};
+
+export default function Navigator({ page }: NavigatorProps) {
+  const { openModal, closeModal } = useLoginModal();
+
+  return (
+    <Container>
+      <BackButton onClick={() => openModal('login', page - 1)}>
+        <IoChevronBack
+          style={{ width: '1.5rem', height: '1.5rem', color: COLORS.gray700 }}
+        />
+      </BackButton>
+      <CloseButton onClick={() => closeModal()}>
+        <CgClose
+          style={{ width: '1.5rem', height: '1.5rem', color: COLORS.gray700 }}
+        />
+      </CloseButton>
+    </Container>
+  );
+}
+
+const Container = styled.div`
+  width: 100%;
+
+  display: flex;
+  justify-content: space-between;
+`;
+
+const BackButton = styled.button`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const CloseButton = styled.button`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
