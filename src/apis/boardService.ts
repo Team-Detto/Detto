c65ecc4e98d197c585cf4e4b0dac3ae374046da7
vpvm96 <vpvm96@gmail.com>
2023-02-14 12:27:35 +0900

    feat: board 수정 삭제 기능 로직 추가

--- src/apis/boardService.ts
+++ src/apis/boardService.ts
@@ -2,28 +2,40 @@ import { firestore } from 'apis/firebaseService';
 import {
   addDoc,
   collection,
+  deleteDoc,
+  doc,
   onSnapshot,
   orderBy,
   query,
+  updateDoc,
 } from 'firebase/firestore';
+import { firebaseImageUploadRequest } from './imageService';
 import { WriteType } from 'types/write/writeType';
 
 export const firebaseCreateProjectRequest = async (
   formData: WriteType.WriteFormType,
   markdownText: string,
+  image: any,
 ) => {
-  await addDoc(collection(firestore, 'post'), {
-    ...formData,
-    startDate: new Date(formData.startDate).getTime(),
-    endDate: new Date(formData.endDate).getTime(),
-    deadline: new Date(formData.deadline).getTime(),
-    content: markdownText,
-    view: 0,
-    like: 0,
-    createdAt: Date.now(),
-    isRecruiting: false,
-    isClosed: false,
-  });
+  try {
+    const sumnailUrl = await firebaseImageUploadRequest(image);
+
+    await addDoc(collection(firestore, 'post'), {
+      ...formData,
+      startDate: new Date(formData.startDate).getTime(),
+      endDate: new Date(formData.endDate).getTime(),
+      deadline: new Date(formData.deadline).getTime(),
+      content: markdownText,
+      sumnail: sumnailUrl,
+      view: 0,
+      like: 0,
+      createdAt: Date.now(),
+      isRecruiting: false,
+      isClosed: false,
+    });
+  } catch (e) {
+    console.error(e);
+  }
 };
 
 // firebase post 데이터 가져오기
@@ -37,4 +49,20 @@ export const firebaseGetProjectDataRequest = (setProjectData: any) => {
     });
     setProjectData(data);
   });
+};
+
+export const firebaseEditProjectRequest = async (
+  id: string,
+  editFormData: any,
+) => {
+  await updateDoc(doc(firestore, 'post', id), {
+    ...editFormData,
+    startDate: new Date(editFormData.startDate).getTime(),
+    endDate: new Date(editFormData.endDate).getTime(),
+    deadline: new Date(editFormData.deadline).getTime(),
+  });
+};
+
+export const firebaseDeleteProjectRequest = async (id: string) => {
+  await deleteDoc(doc(firestore, 'post', id));
 };
