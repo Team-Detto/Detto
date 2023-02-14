094dcb4720d134003e6438ae83e47851543cf44f
vpvm96 <vpvm96@gmail.com>
2023-02-15 00:16:25 +0900

    fix: setState export

--- src/hooks/useWrite.ts
+++ src/hooks/useWrite.ts
@@ -112,6 +112,7 @@ const useWrite = () => {
     editRef,
     imageRef,
     writeFormValue,
+    setWriteFormValue,
     handleModalStateChange,
     handleFormValueChange,
     handleAddThumbnailImage: handleAddThumbnailImage,
