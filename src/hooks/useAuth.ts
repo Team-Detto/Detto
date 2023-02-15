6c415fa43d1b7e6dfde98ccbb9819457a530bf1c
Hyojin <0910.shj@gmail.com>
2023-02-15 14:35:32 +0900

    fix: useAuth 반환 타입 수정

--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -1,14 +1,14 @@
 interface LocalStorageUser {
-  displayName: string;
+  displayName?: string;
   email?: string;
-  photoURL: string;
-  uid: string;
+  photoURL?: string;
+  uid?: string;
 }
 /**
  * 로컬 스토리지에 저장된 사용자 정보를 반환합니다.
- * @returns {LocalStorageUser | undefined}
+ * @returns {LocalStorageUser}
  */
-const useAuth = (): LocalStorageUser | undefined => {
+const useAuth = (): LocalStorageUser => {
   const user = JSON.parse(localStorage.getItem('user') || '{}');
 
   return user;
