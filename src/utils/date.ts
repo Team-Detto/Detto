d4415d832c22ae3f8b6778ba495dac8c16fededc
Hyojin <88768022+su-no@users.noreply.github.com>
2023-02-15 11:17:52 +0900

    Update date.ts

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -1,7 +1,6 @@
 
 // 밀리초 형식의 날짜를 2023.03.02. 포맷으로 변환하는 함수
 export const getDate = (milliseconds: number) => {
-
   const date = new Date(milliseconds);
   const year = `${date.getFullYear()}`.padStart(4, '0');
   const month = `${date.getMonth() + 1}`.padStart(2, '0');
