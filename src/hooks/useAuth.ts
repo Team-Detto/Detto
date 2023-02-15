a840c155dc29d993c6fcd28851b5bcbc0236c83c
Hyojin <0910.shj@gmail.com>
2023-02-15 19:22:04 +0900

    feat: useAuth 커스텀훅 타입 변경

--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -1,8 +1,8 @@
 interface LocalStorageUser {
-  displayName?: string;
-  email?: string;
-  photoURL?: string;
-  uid?: string;
+  displayName: string;
+  email: string;
+  photoURL: string;
+  uid: string;
 }
 /**
  * 로컬 스토리지에 저장된 사용자 정보를 반환합니다.
