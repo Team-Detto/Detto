4eb53ab4c256bb46c261ddacfd5c293958321e0a
Hyojin <0910.shj@gmail.com>
2023-02-16 03:38:59 +0900

    feat: 쪽지함 시간순 정렬

--- src/apis/note.ts
+++ src/apis/note.ts
@@ -4,23 +4,23 @@ import { firestore } from 'apis/firebaseService';
 /**
  * uid를 이용해 받은 쪽지함 목록 조회
  * @param params useQuery Params
- * @returns 사용자의 받은 쪽지함 배열
+ * @returns 사용자의 받은 쪽지함을 시간 내림차순으로 정렬한 배열
  */
 export const getInboxNotes = async (params: any) => {
   const [_, uid] = params.queryKey;
   const docSnap = await getDoc(doc(firestore, `inbox/${uid}`));
   if (!docSnap.exists()) return null;
-  return Object.values(docSnap.data());
+  return Object.values(docSnap.data()).sort((a, b) => b.date - a.date);
 };
 
 /**
  * uid를 이용해 보낸 쪽지함 목록 조회
  * @param params useQuery Params
- * @returns 사용자의 보낸 쪽지함 배열
+ * @returns 사용자의 보낸 쪽지함을 시간 내림차순으로 정렬한 배열
  */
 export const getOutboxNotes = async (params: any) => {
   const [_, uid] = params.queryKey;
   const docSnap = await getDoc(doc(firestore, `outbox/${uid}`));
   if (!docSnap.exists()) return undefined;
-  return Object.values(docSnap.data());
+  return Object.values(docSnap.data()).sort((a, b) => b.date - a.date);
 };
