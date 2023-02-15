65eb0d93c4c2dc993986af5cab03cc6ca16c0c1a
Hyojin <0910.shj@gmail.com>
2023-02-16 02:41:42 +0900

    feat: 쪽지 보내기 기능 구현

--- src/components/popup/SendNote.tsx
+++ src/components/popup/SendNote.tsx
@@ -1,8 +1,9 @@
 import { useQuery } from '@tanstack/react-query';
+import { v4 as uuid } from 'uuid';
 import { firestore } from 'apis/firebaseService';
 import ModalNavigator from 'components/common/modal/ModalNavigator';
 import { doc, updateDoc } from 'firebase/firestore';
-import { useGlobalModal } from 'hooks';
+import { useAuth, useGlobalModal } from 'hooks';
 import CustomButton from './CustomButton';
 import {
   Container,
@@ -12,32 +13,87 @@ import {
   TitleText,
 } from './styles';
 import { getUserInfoData } from 'apis/mypageUsers';
+import { useState } from 'react';
 import styled from '@emotion/styled';
 import COLORS from 'assets/styles/colors';
 
 export default function SendNote({ uid }: { uid: string }) {
+  const [note, setNote] = useState({
+    title: '',
+    content: '',
+  });
+  const [disabled, setDisabled] = useState(false);
+
   const { closeModal } = useGlobalModal();
 
-  const { data: user } = useQuery({
+  const sender = useAuth();
+  const { data: receiver } = useQuery({
     queryKey: ['user', uid],
     queryFn: getUserInfoData,
   });
-  const sendNote = () => {
-    console.log('쪽지 보내기');
-    // updateDoc(doc(firestore, `inbox/${uid}`), {});
+
+  const updateNoteCollection = () => {
+    if (!receiver) return;
+    const noteId = uuid();
+    const date = Date.now();
+
+    return Promise.all([
+      updateDoc(doc(firestore, `inbox/${receiver.uid}`), {
+        [noteId]: {
+          uid: sender.uid,
+          displayName: receiver.displayName,
+          photoURL: receiver.photoURL,
+          date,
+          title: note.title,
+          content: note.content,
+          isRead: false,
+        },
+      }),
+      updateDoc(doc(firestore, `outbox/${sender.uid}`), {
+        [noteId]: {
+          uid: receiver.uid,
+          displayName: sender.displayName,
+          photoURL: sender.photoURL,
+          date,
+          title: note.title,
+          content: note.content,
+          isRead: false,
+        },
+      }),
+    ]);
   };
 
-  if (!user) return null;
+  const handleSendButtonClick = async () => {
+    setDisabled(true);
+    await updateNoteCollection();
+    closeModal();
+  };
+
+  if (!receiver) return null;
   return (
     <Container>
       <ModalNavigator page={0} close />
       <HeaderContainer>
-        <ProfileImage src={user.photoURL} />
-        <TitleText>{user.displayName}님께 쪽지 보내기</TitleText>
+        <ProfileImage src={receiver.photoURL} />
+        <TitleText>{receiver.displayName}님께 쪽지 보내기</TitleText>
       </HeaderContainer>
-      <TitleInput type="text" placeholder="제목을 입력해주세요." autoFocus />
-      <ContentTextarea placeholder="내용을 입력해주세요." />
-      <CustomButton label="쪽지를 보낼게요" onClick={sendNote} />
+      <TitleInput
+        type="text"
+        placeholder="제목을 입력해주세요."
+        autoFocus
+        value={note.title}
+        onChange={(e) => setNote({ ...note, title: e.target.value })}
+      />
+      <ContentTextarea
+        placeholder="내용을 입력해주세요."
+        value={note.content}
+        onChange={(e) => setNote({ ...note, content: e.target.value })}
+      />
+      <CustomButton
+        label="쪽지를 보낼게요"
+        onClick={handleSendButtonClick}
+        disabled={disabled}
+      />
     </Container>
   );
 }
