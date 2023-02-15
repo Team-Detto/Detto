63aef97556bcd8cda6be087d634980b19bb909b6
vpvm96 <vpvm96@gmail.com>
2023-02-15 20:36:40 +0900

    feat: hooks로 비지니스 로직 분리 및 카테고리 기능 적용

---
+++ src/hooks/useFindProject.ts
@@ -0,0 +1,44 @@
+import { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { firebaseGetProjectDataRequest } from 'apis/boardService';
+import { EditType } from 'types/write/writeType';
+
+const useFindProject = () => {
+  const navigate = useNavigate();
+
+  const [projects, setProjects] = useState<EditType.EditFormType[]>([]);
+  const [category, setCategory] = useState<string>('planner');
+  const [toggle, setToggle] = useState(false);
+
+  const filterProjects = projects.filter((project) => {
+    const { positions }: any = project;
+    return !!positions[category];
+  });
+
+  useEffect(() => {
+    firebaseGetProjectDataRequest(setProjects);
+  }, []);
+
+  const handleCategoryClick = (e: any) => {
+    setCategory(e.target.name);
+  };
+
+  const handleToggleClick = () => {
+    setToggle((prev) => !prev);
+  };
+
+  const handleNavigateToProjectDetail = (path: string) => () => {
+    navigate(`/project/${path}`);
+  };
+
+  return {
+    filterProjects,
+    category,
+    toggle,
+    handleCategoryClick,
+    handleToggleClick,
+    handleNavigateToProjectDetail,
+  };
+};
+
+export default useFindProject;
--- src/pages/FindProjectPage.tsx
+++ src/pages/FindProjectPage.tsx
@@ -1,26 +1,18 @@
-import { useEffect, useState } from 'react';
-import { firebaseGetProjectDataRequest } from 'apis/boardService';
+import { useFindProject } from 'hooks';
 import WebContainer from 'components/common/WebContainer';
 import FindProjectHeader from 'components/findproject/FindProjectHeader';
 import FindProjectList from 'components/findproject/FindProjectList';
 import styled from '@emotion/styled';
 
 const FindProjectPage = () => {
-  const [projects, setProjects] = useState([]);
-  const [category, setCategory] = useState('기획');
-  const [toggle, setToggle] = useState(false);
-
-  useEffect(() => {
-    firebaseGetProjectDataRequest(setProjects);
-  }, []);
-
-  const handleCategoryClick = (e: any) => {
-    setCategory(e.target.name);
-  };
-
-  const handleToggleClick = () => {
-    setToggle((prev) => !prev);
-  };
+  const {
+    filterProjects,
+    category,
+    toggle,
+    handleCategoryClick,
+    handleToggleClick,
+    handleNavigateToProjectDetail,
+  } = useFindProject();
 
   return (
     <FindProjectPageWrapper>
@@ -31,7 +23,11 @@ const FindProjectPage = () => {
           onCategoryClickEvent={handleCategoryClick}
           onToggleClickEvent={handleToggleClick}
         />
-        <FindProjectList projects={projects} />
+        <FindProjectList
+          projects={filterProjects}
+          toggle={toggle}
+          onNavigateToProjectDetailEvent={handleNavigateToProjectDetail}
+        />
       </WebContainer>
     </FindProjectPageWrapper>
   );
