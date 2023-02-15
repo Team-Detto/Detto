2f9f8f7a62309087979686e69e19e9afc2a2fae2
Hyojin <0910.shj@gmail.com>
2023-02-16 02:12:21 +0900

    feat: 쪽지 보내기 user 정보 가져오기

--- src/components/popup/SendNote.tsx
+++ src/components/popup/SendNote.tsx
@@ -1,4 +1,7 @@
+import { useQuery } from '@tanstack/react-query';
+import { firestore } from 'apis/firebaseService';
 import ModalNavigator from 'components/common/modal/ModalNavigator';
+import { doc, updateDoc } from 'firebase/firestore';
 import { useGlobalModal } from 'hooks';
 import CustomButton from './CustomButton';
 import {
@@ -8,22 +11,27 @@ import {
   ProfileImage,
   TitleText,
 } from './styles';
+import { getUserInfoData } from 'apis/mypageUsers';
 
-export default function SendNote({ data }: { data: Note }) {
+export default function SendNote({ uid }: { uid: string }) {
   const { closeModal } = useGlobalModal();
 
+  const { data: user } = useQuery({
+    queryKey: ['user', uid],
+    queryFn: getUserInfoData,
+  });
   const sendNote = () => {
     console.log('쪽지 보내기');
     // updateDoc(doc(firestore, `inbox/${uid}`), {});
-    closeModal();
   };
 
+  if (!user) return null;
   return (
     <Container>
       <ModalNavigator page={0} close />
       <HeaderContainer>
-        <ProfileImage src={data.photoURL} />
-        <TitleText>{data.displayName}님께 쪽지 보내기</TitleText>
+        <ProfileImage src={user.photoURL} />
+        <TitleText>{user.displayName}님께 쪽지 보내기</TitleText>
       </HeaderContainer>
       <ContentTextarea autoFocus placeholder="쪽지 내용을 입력해주세요." />
       <CustomButton label="쪽지를 보낼게요" onClick={sendNote} />
