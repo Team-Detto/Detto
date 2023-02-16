e088e8da9ee1e674a452275101cdf9353d860940
Hyojin <0910.shj@gmail.com>
2023-02-16 18:08:46 +0900

    feat: 쪽지함 읽기 db 변경

--- src/apis/note.ts
+++ src/apis/note.ts
@@ -1,26 +1,38 @@
-import { doc, getDoc } from 'firebase/firestore';
+import { collection, query, where, getDocs, orderBy } from 'firebase/firestore';
 import { firestore } from 'apis/firebaseService';
 
 /**
- * uid를 이용해 받은 쪽지함 목록 조회
+ * uid를 이용해 받은 쪽지 목록 조회
  * @param params useQuery Params
- * @returns 사용자의 받은 쪽지함을 시간 내림차순으로 정렬한 배열
+ * @returns 사용자가 받은 쪽지를 시간 내림차순으로 정렬한 배열
  */
 export const getInboxNotes = async (params: any) => {
   const [_, uid] = params.queryKey;
-  const docSnap = await getDoc(doc(firestore, `inbox/${uid}`));
-  if (!docSnap.exists()) return null;
-  return Object.values(docSnap.data()).sort((a, b) => b.date - a.date);
+  const docRef = collection(firestore, `notes`);
+  const q = query(
+    docRef,
+    where('receiverUid', '==', uid),
+    orderBy('date', 'desc'),
+  );
+  const querySnapshot = await getDocs(q);
+  const docs = querySnapshot.docs.map((doc) => ({ ...doc.data(), id: doc.id }));
+  return docs;
 };
 
 /**
- * uid를 이용해 보낸 쪽지함 목록 조회
+ * uid를 이용해 보낸 쪽지 목록 조회
  * @param params useQuery Params
- * @returns 사용자의 보낸 쪽지함을 시간 내림차순으로 정렬한 배열
+ * @returns 사용자가 보낸 쪽지를 시간 내림차순으로 정렬한 배열
  */
 export const getOutboxNotes = async (params: any) => {
   const [_, uid] = params.queryKey;
-  const docSnap = await getDoc(doc(firestore, `outbox/${uid}`));
-  if (!docSnap.exists()) return undefined;
-  return Object.values(docSnap.data()).sort((a, b) => b.date - a.date);
+  const docRef = collection(firestore, `notes`);
+  const q = query(
+    docRef,
+    where('senderUid', '==', uid),
+    orderBy('date', 'desc'),
+  );
+  const querySnapshot = await getDocs(q);
+  const docs = querySnapshot.docs.map((doc) => ({ ...doc.data(), id: doc.id }));
+  return docs;
 };
