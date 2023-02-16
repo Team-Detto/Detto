3675fafb65593f7a306ceb1c90f1dda4c752fc79
LeeYujeong <yujlee242@gmail.com>
2023-02-16 12:28:58 +0900

    feat: 연락처 유효성 검사 추가

--- src/utils/validation.ts
+++ src/utils/validation.ts
@@ -31,3 +31,10 @@ export const nicknameValidation = (nickname: string) => {
   if (nickname.length >= 20) return false;
   return true;
 };
+
+export const contactValidation = (contact: string) => {
+  const emailRegex = /^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/g;
+  if (contact === '') return false;
+  if (!contact.match(emailRegex)) return false;
+  return true;
+};
