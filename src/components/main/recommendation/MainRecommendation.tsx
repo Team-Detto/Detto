68508ce3f8a7953f9b059814ddfd7fd749f0a6a9
Hyojin <0910.shj@gmail.com>
2023-02-17 23:08:28 +0900

    feat: 조회순/관심순 버튼 스타일 변경

--- src/components/main/recommendation/MainRecommendation.tsx
+++ src/components/main/recommendation/MainRecommendation.tsx
@@ -3,15 +3,18 @@ import styled from '@emotion/styled';
 import ContantCard from 'components/ContentCard';
 import { useFindProject } from 'hooks';
 import { useState } from 'react';
+import COLORS from 'assets/styles/colors';
+
+const tapType = [
+  { type: 'inquiry', name: '조회순' },
+  { type: 'attention', name: '관심순' },
+];
 
 const MainRecommendation = () => {
   const { projects, likedProjects, handleNavigateToProjectDetail } =
     useFindProject();
-  const tapType = [
-    { type: 'inquiry', name: '조회순' },
-    { type: 'attention', name: '관심순' },
-  ];
-  const [tap, setTap] = useState('조회순');
+
+  const [tap, setTap] = useState(tapType[0].type);
   return (
     <MainRecommendationWrap>
       <MainRecommendationContainer>
@@ -19,18 +22,17 @@ const MainRecommendation = () => {
           이런 프로젝트는 어때요?
         </MainRecommendationTitle>
         <MainRecommendationButtonContainer>
-          {tapType.map((position) => {
-            return (
-              <MainRecommendationButton
-                key={position.type}
-                name={position.name}
-                value={tap}
-                onClick={() => setTap(position.name)}
-              >
-                {position.name}
-              </MainRecommendationButton>
-            );
-          })}
+          {tapType.map((position) => (
+            <MainRecommendationButton
+              active={position.type === tap}
+              key={position.type}
+              name={position.type}
+              value={tap}
+              onClick={() => setTap(position.type)}
+            >
+              {position.name}
+            </MainRecommendationButton>
+          ))}
         </MainRecommendationButtonContainer>
         <MainRecommendationCardContainer>
           {projects.map(
@@ -74,12 +76,11 @@ const MainRecommendationContainer = styled.div`
 const MainRecommendationTitle = styled.div`
   width: 326px;
   height: 44px;
-  font-family: 'Noto Sans KR';
-  font-style: normal;
+
   font-weight: 700;
   font-size: 32px;
   line-height: 44px;
-  color: #4e5968;
+  color: ${COLORS.gray800};
 `;
 const MainRecommendationButtonContainer = styled.div`
   display: flex;
@@ -90,7 +91,8 @@ const MainRecommendationButtonContainer = styled.div`
   width: 301px;
   height: 48px;
 `;
-const MainRecommendationButton = styled.button`
+
+const MainRecommendationButton = styled.button<{ active: boolean }>`
   box-sizing: border-box;
   display: flex;
   flex-direction: row;
@@ -100,29 +102,15 @@ const MainRecommendationButton = styled.button`
   gap: 10px;
   width: 150px;
   height: 48px;
-  background: ${(props: { name: string; value: string }) =>
-    props.name === props.value ? '#5d50f0' : '#ffffff'};
-  color: ${(props: { name: string; value: string }) =>
-    props.name === props.value ? '#ffffff' : '#4e5968'};
-  border-radius: 36px;
-  font-family: 'Noto Sans KR';
-  font-style: normal;
-  font-weight: 400;
+
+  font-weight: ${(props) => (props.active ? '700' : '400')};
   font-size: 16px;
   line-height: 32px;
 
-  &:hover {
-    width: 150px;
-    height: 48px;
-    background: #5d50f0;
-    border-radius: 36px;
-    color: #ffffff;
-    font-family: 'Noto Sans KR';
-    font-style: normal;
-    font-weight: 700;
-    font-size: 16px;
-    line-height: 32px;
-  }
+  color: ${(props) => (props.active ? COLORS.white : COLORS.black)};
+  background-color: ${(props) =>
+    props.active ? COLORS.violetB400 : COLORS.gray50};
+  border-radius: 36px;
 `;
 const MainRecommendationCardContainer = styled.div`
   display: flex;
@@ -146,8 +134,7 @@ const MainRecommendationCardButton = styled.button`
   border: 1px solid #e1e5eb;
   border-radius: 24px;
   color: #8b95a1;
-  font-family: 'Noto Sans KR';
-  font-style: normal;
+
   font-weight: 400;
   font-size: 16px;
   line-height: 32px;
