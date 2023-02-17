2372dfc61d5896a989614c55fcc54cd539203cb1
Hyojin <0910.shj@gmail.com>
2023-02-17 22:55:50 +0900

    feat: 메인페이지 사용자 조회 useQuery로 변경

--- src/apis/userService.ts
+++ src/apis/userService.ts
@@ -1,8 +1,18 @@
 import { Dispatch } from 'react';
 import { firestore } from './firebaseService';
-import { doc, getDoc } from 'firebase/firestore';
+import { doc, getDoc, getDocs, collection } from 'firebase/firestore';
 
-// // 내 프로젝트 관심 조회
+/**
+ * 파이어베이스 users 컬렉션에서 모든 사용자의 정보를 조회
+ * @returns 모든 사용자의 정보를 담은 배열
+ */
+
+export const firebaseAllUsersRequest = async () => {
+  const querySnapshot = await getDocs(collection(firestore, 'users'));
+  return querySnapshot.docs.map((doc) => doc.data());
+};
+
+// 내 프로젝트 관심 조회
 export const firebaseFindMyInterestRequset = async (
   uid: string,
   setLikedProjects: Dispatch<React.SetStateAction<string[]>>,
--- src/components/main/findUsers/FindUserSlider.tsx
+++ src/components/main/findUsers/FindUserSlider.tsx
@@ -4,70 +4,53 @@ import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 import VectorPrev from '../../../assets/images/VectorPrev.png';
 import VectorNext from '../../../assets/images/VectorNext.png';
-import {
-  collection,
-  doc,
-  getDoc,
-  onSnapshot,
-  orderBy,
-  query,
-} from 'firebase/firestore';
-import { firestore } from 'apis/firebaseService';
-import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
+import { useQuery } from '@tanstack/react-query';
+import { staleTime } from 'utils/staleTime';
+import { firebaseAllUsersRequest } from 'apis/userService';
+
+const settings = {
+  infinite: false,
+  centerPadding: '60px',
+  slidesToShow: 5,
+  slidesToScroll: 1,
+  swipeToSlide: true,
+};
+
 const FindUserSlider = () => {
-  const [users, setUsers] = useState<any>([]);
+  const { data: users } = useQuery({
+    queryKey: ['users'],
+    queryFn: firebaseAllUsersRequest,
+    staleTime: staleTime.users,
+  });
 
-  const firebaseGetProjectDataRequest = (setUsers: any) => {
-    const q = query(collection(firestore, 'users'));
-
-    onSnapshot(q, (querySnapshot) => {
-      const data: any = [];
-      querySnapshot.forEach((doc) => {
-        data.push({ ...doc.data(), id: doc.id });
-      });
-      setUsers(data);
-    });
-  };
-
-  useEffect(() => {
-    firebaseGetProjectDataRequest(setUsers);
-  }, []);
-
-  console.log(users);
-
-  const settings = {
-    infinite: false,
-    centerPadding: '60px',
-    slidesToShow: 5,
-    slidesToScroll: 1,
-    swipeToSlide: true,
-  };
+  if (!users) return null;
   return (
     <SlideArea>
       <StyledSlider {...settings}>
-        {users.map((user: any) => {
-          console.log(user.positions[0]);
-          return (
-            <Link to={'/'}>
-              <Card key={user}>
-                <CardImage src={user.photoURL} />
-                <CardTextContainer>
-                  <CardNickname>{user.displayName}</CardNickname>
-                  <CardJob>{user.positions[0]}</CardJob>
-                </CardTextContainer>
-              </Card>
-            </Link>
-          );
-        })}
+        {users.map((user: any) => (
+          <Link to={'/'}>
+            <Card key={user}>
+              <CardImage src={user.photoURL} />
+              <CardTextContainer>
+                <CardNickname>{user.displayName}</CardNickname>
+                <CardJob>{user.positions[0]}</CardJob>
+              </CardTextContainer>
+            </Card>
+          </Link>
+        ))}
       </StyledSlider>
     </SlideArea>
   );
 };
+
+export default FindUserSlider;
+
 const SlideArea = styled.div`
   width: 1180px;
   height: 201px;
 `;
+
 const StyledSlider = styled(Slider)`
   display: flex;
   flex-direction: row;
@@ -99,6 +82,7 @@ const StyledSlider = styled(Slider)`
     color: #000;
   }
 `;
+
 const Card = styled.div`
   display: flex !important;
   flex-direction: column;
@@ -110,6 +94,7 @@ const Card = styled.div`
   width: 128px;
   height: 201px;
 `;
+
 const CardImage = styled.img`
   background: url('https://noticon-static.tammolo.com/dgggcrkxq/image/upload/v1638101071/noticon/gpr07ptl1x6evhew7li7.png');
   background-size: cover;
@@ -148,4 +133,3 @@ const CardJob = styled.div`
   line-height: 32px;
   text-align: center;
 `;
-export default FindUserSlider;
