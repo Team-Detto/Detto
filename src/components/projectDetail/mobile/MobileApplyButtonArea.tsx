aa70f72b55a377396c58e859d009c14921e308bb
이정은 <2019152052@kpu.ac.kr>
2023-02-20 22:53:40 +0900

    feat: 지원하기 버튼 UI

---
+++ src/components/projectDetail/mobile/MobileApplyButtonArea.tsx
@@ -0,0 +1,35 @@
+import styled from '@emotion/styled';
+import COLORS from 'assets/styles/colors';
+
+const MobileApplyButtonArea = ({ projectData }: any) => {
+  return (
+    <ApplyButtonArea>
+      <ApplyButton>간단 지원하기</ApplyButton>
+    </ApplyButtonArea>
+  );
+};
+
+export default MobileApplyButtonArea;
+
+const ApplyButtonArea = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-top: 3.5rem;
+`;
+
+const ApplyButton = styled.button`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: absolute;
+  width: 17rem;
+  height: 3.75rem;
+  background: ${COLORS.violetB400};
+  border-radius: 0.75rem;
+
+  font-weight: 700;
+  font-size: 1rem;
+
+  color: #ffffff;
+`;
