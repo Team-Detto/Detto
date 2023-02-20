334ddb175779a892513665d2ae72eb9dcf023882
Hyojin <0910.shj@gmail.com>
2023-02-21 01:34:56 +0900

    feat: 모바일 Welcome 페이지

--- src/components/login/mobile/MobileWelcome.tsx
+++ src/components/login/mobile/MobileWelcome.tsx
@@ -1,3 +1,72 @@
+import styled from '@emotion/styled';
+import WELCOME_IMG from 'assets/images/login_welcome.png';
+import COLORS from 'assets/styles/colors';
+import { useGlobalModal } from 'hooks';
+import MobileConfirmButton from './MobileConfirmButton';
+
+// 페이지 4 : 환영합니다!
 export default function MobileWelcome() {
-  return <div></div>;
+  const { closeModal } = useGlobalModal();
+
+  return (
+    <Container>
+      <TopContainer>
+        <KeyImg src={WELCOME_IMG} alt="login" />
+        <TextContainer>
+          <TitleText>Detto에 오신 걸 환영합니다!</TitleText>
+          <SubText>Detto와 함께 즐거운 프로젝트 여정을 만들어보세요</SubText>
+        </TextContainer>
+      </TopContainer>
+      <MobileConfirmButton onClick={closeModal} />
+    </Container>
+  );
 }
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-end;
+
+  width: 100%;
+  height: 100%;
+
+  padding: 1rem;
+`;
+
+const TopContainer = styled.div`
+  margin-bottom: 2.375rem;
+  padding: 0 9px;
+`;
+
+const KeyImg = styled.img`
+  width: 5rem;
+  height: 5rem;
+`;
+
+const TextContainer = styled.div``;
+
+const TitleText = styled.h2`
+  color: ${COLORS.gray850};
+
+  width: 8.0625rem;
+
+  font-weight: 700;
+  font-size: 1.125rem;
+  line-height: 1.625rem;
+
+  margin-bottom: 0.5rem;
+`;
+
+const SubText = styled.h3`
+  width: 14rem;
+
+  font-weight: 500;
+  font-size: 0.875rem;
+  line-height: 140%;
+
+  display: flex;
+  align-items: center;
+  letter-spacing: -0.02em;
+
+  color: ${COLORS.gray750};
+`;
