8d977381455d142158c5e70dcc07841d12e72196
이정은 <2019152052@kpu.ac.kr>
2023-02-20 19:18:43 +0900

    feat: 제목, 썸네일 구역 UI

---
+++ src/components/projectDetail/mobile/MobileTitleThumbnailArea.tsx
@@ -0,0 +1,56 @@
+import styled from '@emotion/styled';
+import COLORS from 'assets/styles/colors';
+
+const MobileTitleThumbnailArea = ({ projectData }: any) => {
+  console.log(projectData?.isRecruiting);
+  return (
+    <>
+      <TitleThumbnailAreaContainer>
+        <IsRecruitingDiv>
+          {projectData?.isRecruiting ? '모집중' : '모집완료'}
+          {/* 스타일 바꾸기 */}
+        </IsRecruitingDiv>
+        <TitleDiv>{projectData?.title}</TitleDiv>
+        {/* 수정, 삭제 추가 */}
+      </TitleThumbnailAreaContainer>
+      <ProjectThumbnail src={projectData?.thumbnail} />
+    </>
+  );
+};
+
+export default MobileTitleThumbnailArea;
+
+const TitleThumbnailAreaContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  /* justify-content: space-between; */
+  align-items: center;
+  width: 100%;
+  height: 28px;
+  padding: 0 20px;
+  gap: 12px;
+`;
+
+const IsRecruitingDiv = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  padding: 0px;
+  gap: 10px;
+  width: 60px;
+  height: 28px;
+  font-size: 14px;
+  background: ${COLORS.violetB400};
+  border-radius: 8px;
+  color: ${COLORS.white};
+`;
+
+const TitleDiv = styled.div``;
+
+const ProjectThumbnail = styled.img`
+  width: 350px;
+  height: 174px;
+  margin: 14px 20px;
+  object-fit: cover;
+`;
