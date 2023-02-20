0775274aabe8779a4f57c02fc09a606533613bd2
Hyojin <0910.shj@gmail.com>
2023-02-20 15:38:28 +0900

    feat: 로그인 모달창 사이즈 조절

--- src/components/login/LoginModal.tsx
+++ src/components/login/LoginModal.tsx
@@ -1,4 +1,4 @@
-import { useGlobalModal } from 'hooks';
+import { useGlobalModal, useIsMobile } from 'hooks';
 import { useEffect } from 'react';
 import {
   SocialLogin,
@@ -10,6 +10,8 @@ import {
 import { allowScroll, preventScroll } from 'utils/modal';
 
 export default function LoginModal() {
+  const isMobile = useIsMobile();
+
   const {
     modal: { page },
     updateModalSize,
@@ -17,7 +19,11 @@ export default function LoginModal() {
 
   // 페이지에 따라 모달 크기 조절
   useEffect(() => {
-    updateModalSize(modals[page].width, modals[page].height);
+    if (isMobile) {
+      updateModalSize(modals[page].mobileWidth, modals[page].mobileHeight);
+    } else {
+      updateModalSize(modals[page].width, modals[page].height);
+    }
   }, [page]);
 
   // 모달이 열려있을 때 body 스크롤 방지
@@ -36,30 +42,40 @@ const modals = [
     // 페이지 0 : 로그인
     width: '41.0625rem',
     height: '31.4375rem',
+    mobileWidth: '20rem',
+    mobileHeight: '22rem',
     component: <SocialLogin />,
   },
   {
     // 페이지 1 : 포지션 선택
     width: '44.25rem',
     height: '36.375rem',
+    mobileWidth: '20rem',
+    mobileHeight: '22rem',
     component: <SetPositions />,
   },
   {
     // 페이지 2 : 기술스택 선택
     width: '70rem',
     height: '46.625rem',
+    mobileWidth: '20rem',
+    mobileHeight: '26.1875rem',
     component: <SetSkills />,
   },
   {
     // 페이지 3 : 프로필 사진, 닉네임 변경
     width: '47.8125rem',
     height: '38.5625rem',
+    mobileWidth: '20rem',
+    mobileHeight: '26.1875rem',
     component: <SetProfile />,
   },
   {
     // 페이지 4 : 환영합니다!
     width: '37.5625rem',
     height: '22.9375rem',
+    mobileWidth: '20rem',
+    mobileHeight: '21.5rem',
     component: <Welcome />,
   },
 ];
