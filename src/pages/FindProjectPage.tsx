01d2e463464eaa3d2b243c1c5d4f14bbeabc396d
vpvm96 <vpvm96@gmail.com>
2023-02-21 22:46:09 +0900

    feat: 프로젝트 찾기 적응형 UI 완성

--- src/pages/FindProjectPage.tsx
+++ src/pages/FindProjectPage.tsx
@@ -1,7 +1,9 @@
-import { useFindProject } from 'hooks';
+import { useFindProject, useIsMobile } from 'hooks';
 import WebContainer from 'components/common/WebContainer';
 import FindProjectHeader from 'components/findproject/FindProjectHeader';
 import FindProjectList from 'components/findproject/FindProjectList';
+import FindProjectMobileHeader from 'components/findproject/mobile/FindProjectMobileHeader';
+import FindProjectMobileList from 'components/findproject/mobile/FindProjectMobileList';
 import styled from '@emotion/styled';
 
 const FindProjectPage = () => {
@@ -14,6 +16,27 @@ const FindProjectPage = () => {
     handleToggleClick,
     handleNavigateToProjectDetail,
   } = useFindProject();
+  const isMobile = useIsMobile();
+
+  if (isMobile) {
+    return (
+      <FindProjectMobilePageContainer>
+        <FindProjectMobileHeader
+          toggle={toggle}
+          category={category}
+          onCategoryClickEvent={handleCategoryClick}
+          onToggleClickEvent={handleToggleClick}
+        />
+        <FindProjectMobileList
+          projects={projects}
+          likedProjects={likedProjects}
+          toggle={toggle}
+          category={category}
+          onNavigateToProjectDetailEvent={handleNavigateToProjectDetail}
+        />
+      </FindProjectMobilePageContainer>
+    );
+  }
 
   return (
     <FindProjectPageWrapper>
@@ -40,6 +63,11 @@ const FindProjectPageWrapper = styled.div`
   width: 100%;
   height: 100%;
   padding-bottom: 10rem;
+`;
+const FindProjectMobilePageContainer = styled.div`
+  max-width: 500px;
+  min-width: 350px;
+  height: 55.875rem;
 `;
 
 export default FindProjectPage;
