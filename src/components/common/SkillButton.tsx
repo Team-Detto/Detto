cd9f5a9baffdde6f0f2c2010ff304e18f9c0996c
vpvm96 <vpvm96@gmail.com>
2023-02-21 15:56:20 +0900

    feat: Mobile용 스킬버튼 추가

--- src/components/common/SkillButton.tsx
+++ src/components/common/SkillButton.tsx
@@ -1,4 +1,5 @@
 import { memo, useState, useEffect, useCallback } from 'react';
+import { useIsMobile } from 'hooks';
 import { useSetRecoilState } from 'recoil';
 import { mypageInfoButtonActiveState } from '../../recoil/atoms';
 import styled from '@emotion/styled';
@@ -15,6 +16,7 @@ interface props {
 const SkillButton = ({ name, value, isChecked, setValue, type }: props) => {
   const [isActive, setIsActive] = useState(false);
   const setActiveInfoButton = useSetRecoilState(mypageInfoButtonActiveState);
+  const isMobile = useIsMobile();
 
   const handleActiveButton = useCallback(() => {
     setActiveInfoButton(true);
@@ -38,11 +40,33 @@ const SkillButton = ({ name, value, isChecked, setValue, type }: props) => {
     });
   }, [name, setValue, type, value]);
 
+  const handleCancelButtonMobile = useCallback(() => {
+    if (!type) return;
+    if (!setValue) return;
+    if (['plannerStack', 'designerStack', 'developerStack'].includes(type)) {
+      setValue((prev: any) => ({
+        ...prev,
+        [type]: prev[type].filter((item: string) => item !== name),
+      }));
+    }
+  }, [type, name, setValue]);
+
   useEffect(() => {
     if (isChecked) {
       setIsActive(true);
     }
   }, [isChecked]);
+
+  if (isMobile) {
+    return (
+      <WritePageMobileStackButtonBox>
+        <WritePageMobileStackSkill>{name}</WritePageMobileStackSkill>
+        <WritePageMobileStackSkillCancel onClick={handleCancelButtonMobile}>
+          ⅹ
+        </WritePageMobileStackSkillCancel>
+      </WritePageMobileStackButtonBox>
+    );
+  }
 
   return (
     <SkillButtonContainer>
@@ -59,22 +83,48 @@ const SkillBtn = styled.button`
     props.isActive === true ? `${COLORS.violetB500}` : `${COLORS.gray100}`};
   color: ${(props: { isActive: boolean }) =>
     props.isActive === true ? `${COLORS.white}` : `${COLORS.black}`};
-
   font-weight: 400;
-  font-size: 12px;
-  line-height: 32px;
-
+  font-size: 0.75rem;
+  line-height: 2rem;
   height: 2rem;
-
   display: flex;
   align-items: center;
-
-  border-radius: 32px;
-  padding: 5px 15px;
+  border-radius: 2rem;
+  padding: 0.3125rem 0.9375rem;
   transition: transform 0.5s;
   &:hover {
     transform: scale(1.05);
   }
+`;
+const WritePageMobileStackButtonBox = styled.div`
+  width: 100%;
+  display: flex;
+  align-items: center;
+  gap: 0.1rem;
+`;
+const WritePageMobileStackSkill = styled.button`
+  width: 4.6rem;
+  height: 1.6rem;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 0px 12px;
+  font-weight: 400;
+  font-size: 0.625rem;
+  line-height: 2rem;
+  border-radius: 12px;
+  color: ${COLORS.black};
+  background-color: ${COLORS.gray100};
+`;
+const WritePageMobileStackSkillCancel = styled.button`
+  width: 1rem;
+  height: 1rem;
+  border-radius: 3.125rem;
+  font-weight: 500;
+  font-size: 0.625rem;
+  padding-bottom: 0.3rem;
+  color: ${COLORS.white};
+  background-color: ${COLORS.gray300};
 `;
 
 export default memo(SkillButton);
