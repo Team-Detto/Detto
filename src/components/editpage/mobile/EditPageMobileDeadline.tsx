5f9dd331a4f2e94cb076d7f5ae3f4a01cdcc4aba
vpvm96 <vpvm96@gmail.com>
2023-02-21 18:53:17 +0900

    feat: 글 수정 Mobile 마김일 UI 완성

---
+++ src/components/editpage/mobile/EditPageMobileDeadline.tsx
@@ -0,0 +1,58 @@
+import { ChangeEvent } from 'react';
+import {
+  EditPageMobileBodyLeftBox,
+  EditPageMobileBodyRightBox,
+  EditPageMobileBodyText,
+} from './EditPageMobileBody';
+import COLORS from 'assets/styles/colors';
+import styled from '@emotion/styled';
+
+interface Props {
+  deadline: number;
+  onFormValueChangeEvent: (e: ChangeEvent<HTMLInputElement>) => void;
+}
+
+const EditPageMobileDeadline = ({
+  deadline,
+  onFormValueChangeEvent,
+}: Props) => {
+  return (
+    <EditPageMobileDeadlineContainer>
+      <EditPageMobileBodyLeftBox>
+        <EditPageMobileBodyText>모집 마감일</EditPageMobileBodyText>
+      </EditPageMobileBodyLeftBox>
+      <EditPageMobileBodyRightBox>
+        <EditPageMobileDeadlineInput
+          type="date"
+          name="deadline"
+          value={new Date(+new Date(deadline)).toISOString().split('T')[0]}
+          onChange={onFormValueChangeEvent}
+        />
+      </EditPageMobileBodyRightBox>
+    </EditPageMobileDeadlineContainer>
+  );
+};
+
+const EditPageMobileDeadlineContainer = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+`;
+const EditPageMobileDeadlineInput = styled.input`
+  width: 7.0625rem;
+  height: 2.75rem;
+  background-color: ${COLORS.white};
+  border: 0.0625rem solid ${COLORS.gray100};
+  border-radius: 0.25rem;
+
+  font-size: 0.75rem;
+  font-weight: 400;
+  line-height: 140%;
+  color: ${COLORS.black};
+
+  text-align: center;
+  padding-right: 0.5rem;
+`;
+
+export default EditPageMobileDeadline;
