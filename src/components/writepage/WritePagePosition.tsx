94b1043f3829379767cf5d434dd44f58bcd864cf
vpvm96 <vpvm96@gmail.com>
2023-02-22 13:58:46 +0900

    fix: 불필요한 로직 제거

--- src/components/writepage/WritePagePosition.tsx
+++ src/components/writepage/WritePagePosition.tsx
@@ -1,10 +1,10 @@
-import React from 'react';
+import { ChangeEvent } from 'react';
 import { WriteType } from 'types/write/writeType';
 import LabelInput from 'components/common/LabelInput';
 import styled from '@emotion/styled';
 interface Props {
   writeFormValue: WriteType.WriteFormType;
-  onFormValueChangeEvent: (e: React.ChangeEvent<HTMLInputElement>) => void; // eslint-disable-line no-unused-vars
+  onFormValueChangeEvent: (e: ChangeEvent<HTMLInputElement>) => void;
 }
 
 const WritePagePosition = ({
