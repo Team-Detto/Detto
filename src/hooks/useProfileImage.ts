639c9c5efa4bc84527245ee7e0e281291ac43c1d
LeeYujeong <yujlee242@gmail.com>
2023-02-22 11:57:49 +0900

    fix: 프로필 사진 업로드 시 이미지 압축 추가

--- src/hooks/useProfileImage.ts
+++ src/hooks/useProfileImage.ts
@@ -1,17 +1,27 @@
 import { useState, useEffect } from 'react';
+import Resizer from 'react-image-file-resizer';
 import { deleteProfileImg, uploadProfileImg } from 'apis/mypageUsers';
 
 const useProfileImage = (uid: string, photoURL?: string) => {
   const [profileImg, setProfileImg] = useState<string>('');
 
   // input type="file"의 onChange 이벤트 핸들러
-  const handleProfileImageChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const file = e.target.files?.[0];
+  const handleProfileImageChange = async (
+    e: React.ChangeEvent<HTMLInputElement>,
+  ) => {
+    if (!e.target.files) {
+      return;
+    }
+    const file = e.target.files[0];
     const reader = new FileReader();
     reader.readAsDataURL(file as Blob);
 
+    const resizedImage = await resizeFile(file);
+
     // 이미지를 스토리지에 업로드 후 반환받은 이미지 url을 상태(profileImg)로 저장
-    uploadProfileImg(file, uid).then((res) => setProfileImg(res));
+    uploadProfileImg(file, uid).then((res) =>
+      setProfileImg(resizedImage as string),
+    );
   };
 
   // 이미지 삭제 이벤트 핸들러
@@ -33,3 +43,19 @@ const useProfileImage = (uid: string, photoURL?: string) => {
 };
 
 export default useProfileImage;
+
+const resizeFile = (file: File) =>
+  new Promise((resolve) => {
+    Resizer.imageFileResizer(
+      file,
+      200,
+      200,
+      'JPEG',
+      70,
+      0,
+      (uri) => {
+        resolve(uri);
+      },
+      'base64',
+    );
+  });
