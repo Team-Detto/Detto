ecf0abca248735a451d2c2c945b321defebb62dd
LeeYujeong <yujlee242@gmail.com>
2023-02-23 11:34:15 +0900

    fix: 모바일 푸터 팀원찾기 페이지에서 가려지도록 수정

--- src/components/MobileFooter.tsx
+++ src/components/MobileFooter.tsx
@@ -1,11 +1,14 @@
 import styled from '@emotion/styled';
 import COLORS from 'assets/styles/colors';
-import { OurTeamText } from './Footer';
+import { useLocation } from 'react-router-dom';
 
 const MobileFooter = () => {
+  const { pathname } = useLocation();
+  const isFindProject = pathname.includes('findproject');
+
   return (
     // TODO:: 푸터 디자인 후 UI 추가 작업 필요
-    <FooterContainer>
+    <FooterContainer isFindeProject={isFindProject}>
       <CopyRightText>@2023 All Rights Reserved</CopyRightText>
     </FooterContainer>
   );
@@ -13,12 +16,16 @@ const MobileFooter = () => {
 
 export default MobileFooter;
 
-const FooterContainer = styled.footer`
+const FooterContainer = styled.footer<{ isFindeProject: boolean }>`
   width: 100%;
   height: 3rem;
   background-color: ${COLORS.gray50};
   margin-top: 7rem;
   padding: 0 1.5rem;
+  position: relative;
+  left: 0;
+  bottom: 0;
+  display: ${({ isFindeProject }) => (isFindeProject ? 'none' : 'block')};
 `;
 
 const CopyRightText = styled.p`
