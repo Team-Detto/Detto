bf4fe34f572ee4b51e413818a10265ce4e42c4ee
이정은 <2019152052@kpu.ac.kr>
2023-02-27 16:58:32 +0900

    fix: 팀원찾기 페이지 헬멧 적용

--- src/pages/FindProjectPage.tsx
+++ src/pages/FindProjectPage.tsx
@@ -7,6 +7,8 @@ import FindProjectList from 'components/findproject/FindProjectList';
 import FindProjectMobileHeader from 'components/findproject/mobile/FindProjectMobileHeader';
 import FindProjectMobileList from 'components/findproject/mobile/FindProjectMobileList';
 import styled from '@emotion/styled';
+import { Helmet } from 'react-helmet-async';
+import mobilesecondBanner from 'assets/images/mobilesecondBanner.png';
 
 const FindProjectPage = () => {
   const {
@@ -31,42 +33,100 @@ const FindProjectPage = () => {
 
   if (isMobile) {
     return (
-      <FindProjectMobilePageContainer>
-        <FindProjectMobileHeader
-          toggle={toggle}
-          category={category}
-          onCategoryClickEvent={handleCategoryClick}
-          onToggleClickEvent={handleToggleClick}
-        />
-        <FindProjectMobileList
-          projects={projects}
-          likedProjects={likedProjects}
-          toggle={toggle}
-          category={category}
-          onNavigateToProjectDetailEvent={handleNavigateToProjectDetail}
-        />
-      </FindProjectMobilePageContainer>
+      <>
+        <Helmet>
+          <title>{`팀원찾기 - Detto`}</title>
+
+          <meta
+            name="description"
+            content="개발자를 위한 사이드 프로젝트 팀 매칭 플랫폼, Detto (Develop Together)"
+          />
+
+          <meta property="og:type" content="website" />
+          <meta property="og:title" content={`팀원찾기 - Detto`} />
+          <meta property="og:site_name" content="Detto" />
+          <meta
+            property="og:description"
+            content="개발자를 위한 사이드 프로젝트 팀 매칭 플랫폼, Detto (Develop Together)"
+          />
+          <meta property="og:image" content={mobilesecondBanner} />
+          <meta property="og:url" content={window.location.href} />
+
+          <meta name="twitter:title" content={`팀원찾기 - Detto`} />
+          <meta
+            name="twitter:description"
+            content="개발자를 위한 사이드 프로젝트 팀 매칭 플랫폼, Detto (Develop Together)"
+          />
+          <meta name="twitter:image" content={mobilesecondBanner} />
+
+          <link rel="canonical" href={window.location.href} />
+        </Helmet>
+        <FindProjectMobilePageContainer>
+          <FindProjectMobileHeader
+            toggle={toggle}
+            category={category}
+            onCategoryClickEvent={handleCategoryClick}
+            onToggleClickEvent={handleToggleClick}
+          />
+          <FindProjectMobileList
+            projects={projects}
+            likedProjects={likedProjects}
+            toggle={toggle}
+            category={category}
+            onNavigateToProjectDetailEvent={handleNavigateToProjectDetail}
+          />
+        </FindProjectMobilePageContainer>
+      </>
     );
   }
 
   return (
-    <FindProjectPageWrapper>
-      <WebContainer>
-        <FindProjectHeader
-          toggle={toggle}
-          category={category}
-          onCategoryClickEvent={handleCategoryClick}
-          onToggleClickEvent={handleToggleClick}
+    <>
+      <Helmet>
+        <title>{`팀원찾기 - Detto`}</title>
+
+        <meta
+          name="description"
+          content="개발자를 위한 사이드 프로젝트 팀 매칭 플랫폼, Detto (Develop Together)"
         />
-        <FindProjectList
-          projects={projects}
-          likedProjects={likedProjects}
-          toggle={toggle}
-          category={category}
-          onNavigateToProjectDetailEvent={handleNavigateToProjectDetail}
+
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content={`팀원찾기 - Detto`} />
+        <meta property="og:site_name" content="Detto" />
+        <meta
+          property="og:description"
+          content="개발자를 위한 사이드 프로젝트 팀 매칭 플랫폼, Detto (Develop Together)"
         />
-      </WebContainer>
-    </FindProjectPageWrapper>
+        <meta property="og:image" content={mobilesecondBanner} />
+        <meta property="og:url" content={window.location.href} />
+
+        <meta name="twitter:title" content={`팀원찾기 - Detto`} />
+        <meta
+          name="twitter:description"
+          content="개발자를 위한 사이드 프로젝트 팀 매칭 플랫폼, Detto (Develop Together)"
+        />
+        <meta name="twitter:image" content={mobilesecondBanner} />
+
+        <link rel="canonical" href={window.location.href} />
+      </Helmet>
+      <FindProjectPageWrapper>
+        <WebContainer>
+          <FindProjectHeader
+            toggle={toggle}
+            category={category}
+            onCategoryClickEvent={handleCategoryClick}
+            onToggleClickEvent={handleToggleClick}
+          />
+          <FindProjectList
+            projects={projects}
+            likedProjects={likedProjects}
+            toggle={toggle}
+            category={category}
+            onNavigateToProjectDetailEvent={handleNavigateToProjectDetail}
+          />
+        </WebContainer>
+      </FindProjectPageWrapper>
+    </>
   );
 };
 
