20ffa5bed74d9aec341adf71e9681e388bcb721b
LeeYujeong <yujlee242@gmail.com>
2023-03-01 21:53:30 +0900

    refactor: useHeader에서 useGradient 훅 분리

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -14,6 +14,7 @@ import useProjectList from './useProjectList';
 import useToastPopup from './useToastPopup';
 import useUpdateProfile from './useUpdateProfile';
 import useWrite from './useWrite';
+import useGradient from './useGradient';
 
 export {
   useAuth,
@@ -32,4 +33,5 @@ export {
   useToastPopup,
   useUpdateProfile,
   useWrite,
+  useGradient,
 };
